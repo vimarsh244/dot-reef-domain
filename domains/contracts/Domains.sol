a66de3d33c539b07f8199110f46ca265aff415d2
Vimarsh <vimarsh244@gmail.com>
2022-11-27 14:28:37 +0000

    hmmm

--- domains/contracts/Domains.sol
+++ domains/contracts/Domains.sol
@@ -1,11 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("hellow from 2077");
+  // Here's our domain TLD!
+  string public tld;
+
+  mapping(string => address) public domains;
+  mapping(string => string) public records;
+		
+  // We make the contract "payable" by adding this to the constructor
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
   }
+		
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 50 * 10**18; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 30 * 10**18; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 10 * 10**18;
+    }
+  }
+  // Added "payable" modifier to register function
+  function register(string calldata name) public payable{
+    require(domains[name] == address(0));
+    
+    uint _price = price(name);
+
+    // Check if enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough reef paid");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+  // Other functions unchanged
+
+  function getAddress(string calldata name) public view returns (address) {
+      return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
+  }
+
+
 }
--- domains/scripts/run.js
+++ domains/scripts/run.js
@@ -1,9 +1,21 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    // We pass in "ninja" to the constructor when deploying
+    const domainContract = await domainContractFactory.deploy("rf");
     await domainContract.deployed();
+  
     console.log("Contract deployed to:", domainContract.address);
-  };
+  
+    // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+    let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('50')});
+    await txn.wait();
+  
+    const address = await domainContract.getAddress("mortal");
+    console.log("Owner of domain mortal:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  }
   
   const runMain = async () => {
     try {
