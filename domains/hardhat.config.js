6eb07c59cf4b40e6c77092760a7dff805ad4c00f
Vimarsh <vimarsh244@gmail.com>
2022-11-27 21:14:09 +0000

    env variables for secret key etc

--- domains/hardhat.config.js
+++ domains/hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    // Mainnet reef network configuration
+    reef_mainnet: {
+      url: "wss://rpc.reefscan.com/ws",
+      scanUrl: "https://reefscan.com",
+      seeds: {
+        "ac1": env.private_key_mainnet,
+      }
+    },
+    // Testnet reef network configuration
+    reef_testnet: {
+      url: "wss://rpc-testnet.reefscan.com/ws",
+      scanUrl: "https://reefscan.com",
+      seeds: {
+        "ac1": env.private_key_testnet,
+      }
+    },
+    // Localhost reef network configuration
+    reef: {
+      url: "ws://127.0.0.1:9944",
+      scanUrl: "http://localhost:3000",
+      seeds: {
+        "ac1": env.private_key_mainnet,
+      }
+    },
+  },
 };
