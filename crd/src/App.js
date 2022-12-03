d9861d2bcf5a8f66fe917fdfbe947e457b773993
Vimarsh <vimarsh244@gmail.com>
2022-12-03 08:27:58 +0000

    changes

--- crd/src/App.js
+++ crd/src/App.js
@@ -6,261 +6,181 @@ import { Contract } from "ethers";
 import GreeterContract from "./contracts/Greeter.json";
 import Uik from "@reef-defi/ui-kit";
 
-
-import Navbar from './components/Navbar';
-
-
 const FactoryAbi = GreeterContract.abi;
 const factoryContractAddress = GreeterContract.address;
 
 const URL = "wss://rpc-testnet.reefscan.com/ws";
 
-
-
-
-
-
 function App() {
-	const [msgVal, setMsgVal] = useState("");
-	const [msg, setMsg] = useState("");
-	const [signer, setSigner] = useState();
-	const [isWalletConnected, setWalletConnected] = useState(false);
+  const [msgVal, setMsgVal] = useState("");
+  const [msg, setMsg] = useState("");
+  const [signer, setSigner] = useState();
+  const [isWalletConnected, setWalletConnected] = useState(false);
 
-	const checkExtension = async () => {
-		let allInjected = await web3Enable("Reef");
+  const [secondTab, setSecondTab] = useState("mint");
 
-		if (allInjected.length === 0) {
-			return false;
-		}
+  const checkExtension = async () => {
+    let allInjected = await web3Enable("Reef");
 
-		let injected;
-		if (allInjected[0] && allInjected[0].signer) {
-			injected = allInjected[0].signer;
-		}
+    if (allInjected.length === 0) {
+      return false;
+    }
 
-		const evmProvider = new Provider({
-			provider: new WsProvider(URL),
-		});
+    let injected;
+    if (allInjected[0] && allInjected[0].signer) {
+      injected = allInjected[0].signer;
+    }
 
-		evmProvider.api.on("ready", async () => {
-			const allAccounts = await web3Accounts();
+    const evmProvider = new Provider({
+      provider: new WsProvider(URL),
+    });
 
-			allAccounts[0] &&
-				allAccounts[0].address &&
-				setWalletConnected(true);
+    evmProvider.api.on("ready", async () => {
+      const allAccounts = await web3Accounts();
 
-			console.log(allAccounts);
+      allAccounts[0] && allAccounts[0].address && setWalletConnected(true);
 
-			const wallet = new Signer(
-				evmProvider,
-				allAccounts[0].address,
-				injected
-			);
+      console.log(allAccounts);
 
-			// Claim default account
-			if (!(await wallet.isClaimed())) {
-				console.log(
-					"No claimed EVM account found -> claimed default EVM account: ",
-					await wallet.getAddress()
-				);
-				await wallet.claimDefaultAccount();
-			}
+      const wallet = new Signer(evmProvider, allAccounts[0].address, injected);
 
-			setSigner(wallet);
-		});
-	};
+      // Claim default account
+      if (!(await wallet.isClaimed())) {
+        console.log(
+          "No claimed EVM account found -> claimed default EVM account: ",
+          await wallet.getAddress()
+        );
+        await wallet.claimDefaultAccount();
+      }
 
-	const checkSigner = async () => {
-		if (!signer) {
-			await checkExtension();
-		}
-		return true;
-	};
+      setSigner(wallet);
+    });
+  };
 
-	const getGreeting = async () => {
-		await checkSigner();
-		const factoryContract = new Contract(
-			factoryContractAddress,
-			FactoryAbi,
-			signer
-		);
-		const result = await factoryContract.greet();
-		setMsg(result);
-	};
+  const checkSigner = async () => {
+    if (!signer) {
+      await checkExtension();
+    }
+    return true;
+  };
 
-	const setGreeting = async () => {
-		await checkSigner();
-		const factoryContract = new Contract(
-			factoryContractAddress,
-			FactoryAbi,
-			signer
-		);
-		await factoryContract.setGreeting(msgVal);
-		setMsgVal("");
-		getGreeting();
-	};
+  const getGreeting = async () => {
+    await checkSigner();
+    const factoryContract = new Contract(
+      factoryContractAddress,
+      FactoryAbi,
+      signer
+    );
+    const result = await factoryContract.greet();
+    setMsg(result);
+  };
 
-	return (
-		<Uik.Container className="fancy">
-			 <Navbar />
-		<Uik.Container className="main">
-			<Uik.Container vertical>
-				<Uik.Container>
-					<Uik.Text className = "head1" text="get a .reef domain" type="headline" />
-				</Uik.Container>
-				{isWalletConnected ? (
-					<Uik.Container vertical className="container">
-						<Uik.Divider text="Get Message from Contract" />
-						<Uik.Card condensed>
-							<Uik.Container>
-								<Uik.Input
-									onChange={e => setMsgVal(e.target.value)}
-									value={msgVal}
-								/>
-								<Uik.Button
-									onKeyPress={e => {
-										e.key === "Enter" && setGreeting();
-									}}
-									onClick={setGreeting}
-									text="Set message"
-									className="container-button"
-								/>
-							</Uik.Container>
-						</Uik.Card>
-						<Uik.Divider text="Set Message to Contract" />
-						<Uik.Card condensed>
-							<Uik.Container flow="spaceBetween">
-								<Uik.Text
-									text={
-										msg.length
-											? msg
-											: "Nothing on contract yet"
-									}
-									type={msg.length ? "lead" : "light"}
-								/>
-								<Uik.Button
-									className="container-button"
-									onClick={getGreeting}
-									text="Get Message"
-								/>
-							</Uik.Container>
-						</Uik.Card>
-					</Uik.Container>
-				) : (
-					<>
-						<Uik.Container vertical className="container">
-							<Uik.Text
-								className="Text"
-								text="create your own .reef domain"
-								type="title"
-							/>
-							<Uik.Text
-								text="We've gone ahead and added essential Reef components to quickstart your Dapp 🪸"
-								type="light"
-							/>
-							<br />
-							<Uik.Text
-								type="light"
-								text={
-									<>
-										<a
-											target="_blank"
-											rel="noreferrer"
-											href="https://ui-kit.reef.io"
-										>
-											Reef UI Kit:
-										</a>
-										<span>
-											{" "}
-											Reef's official component library to
-											help you make beautiful dapps with
-											ease
-										</span>
-									</>
-								}
-							/>
-							<Uik.Text
-								type="light"
-								text={
-									<>
-										<a
-											target="_blank"
-											rel="noreferrer"
-											href="https://github.com/reef-defi/evm-provider.js"
-										>
-											Reef EVM Provider:
-										</a>
-										<span>
-											{" "}
-											Implements a web3 provider which can
-											interact with the Reef chain EVM.
-										</span>
-									</>
-								}
-							/>
-							<Uik.Text
-								type="light"
-								text={
-									<>
-										<a
-											target="_blank"
-											rel="noreferrer"
-											href="https://create-react-app.dev/"
-										>
-											Create React App:
-										</a>
-										<span>
-											{" "}
-											Create React App is the best way to
-											start building a new single-page
-											application in React.
-										</span>
-									</>
-								}
-							/>
-							<Uik.Text
-								type="light"
-								text={
-									<>
-										<a
-											target="_blank"
-											rel="noreferrer"
-											href="https://github.com/dilanx/craco"
-										>
-											Craco:
-										</a>
-										<span>
-											{" "}
-											Craco helps you leverage the power
-											of webpack and babel configurations
-											without ejecting your CRA template
-										</span>
-									</>
-								}
-							/>
-							<br />
-							<Uik.Text
-								text={
-									<>
-										Click the{" "}
-										<Uik.Tag>Connect Wallet</Uik.Tag> button
-										to get started 🚀
-									</>
-								}
-								type="light"
-							/>
-						</Uik.Container>
-						<br />
-						<Uik.Button
-							text="Connect Wallet"
-							onClick={checkExtension}
-						/>
-					</>
-				)}
-			</Uik.Container>
-		</Uik.Container>
-		</Uik.Container>
-	);
+  const setGreeting = async () => {
+    await checkSigner();
+    const factoryContract = new Contract(
+      factoryContractAddress,
+      FactoryAbi,
+      signer
+    );
+    await factoryContract.setGreeting(msgVal);
+    setMsgVal("");
+    getGreeting();
+  };
+
+  return (
+    <Uik.Container className="main">
+      <Uik.Container vertical>
+        <Uik.Container>
+          <Uik.Text text=".reef domains" type="headline" />
+        </Uik.Container>
+
+        <Uik.Tabs
+          value={secondTab}
+          onChange={(value) => setSecondTab(value)}
+          options={[
+            { value: "mint", text: "Mint" },
+            { value: "view", text: "View" },
+            { value: "update", text: "Update" },
+            
+            { value: "resolve", text: "Resolve" },
+          ]}
+        />
+
+        {isWalletConnected ? (
+          <Uik.Container vertical className="container">
+            <Uik.Divider text="Get Message from Contract" />
+            <Uik.Card condensed>
+              <Uik.Container>
+                <Uik.Input
+                  onChange={(e) => setMsgVal(e.target.value)}
+                  value={msgVal}
+                />
+                <Uik.Button
+                  onKeyPress={(e) => {
+                    e.key === "Enter" && setGreeting();
+                  }}
+                  onClick={setGreeting}
+                  text="Set message"
+                  className="container-button"
+                />
+              </Uik.Container>
+
+
+
+            </Uik.Card>
+            <Uik.Divider text="Set Message to Contract" />
+            <Uik.Card condensed>
+              <Uik.Container flow="spaceBetween">
+                <Uik.Text
+                  text={msg.length ? msg : "Nothing on contract yet"}
+                  type={msg.length ? "lead" : "light"}
+                />
+                <Uik.Button
+                  className="container-button"
+                  onClick={getGreeting}
+                  text="Get Message"
+                />
+              </Uik.Container>
+            </Uik.Card>
+          </Uik.Container>
+        ) : (
+          <>
+            <Uik.Container vertical className="container">
+              <Uik.Text
+                className="title"
+                text="Create your own 
+				
+				<i>.reef</i> domains"
+                type="title"
+              />
+
+
+{/* 
+			<Uik.Text
+                className="title"
+                text="Create your own "
+				type="title"
+				style="uk-text-italic"
+
+			/> */}
+
+
+              <Uik.Text
+                text={
+                  <>
+                    Link your <Uik.Tag>Wallet</Uik.Tag> get started 🚀
+                  </>
+                }
+                type="light"
+              />
+            </Uik.Container>
+            <br />
+            <Uik.Button text="Connect Wallet" onClick={checkExtension} />
+          </>
+        )}
+      </Uik.Container>
+    </Uik.Container>
+  );
 }
 
 export default App;
